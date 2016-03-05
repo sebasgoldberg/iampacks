f8bac151382cfc8b4b9c869cb4ad35a89ac361af
Sebastian Goldberg <sebas.goldberg@gmail.com>
2016-03-05 20:46:10 -0300

    setup.py updated with iampacks module.

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['cross', 'agencia',],
+    packages=['iampacks',],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
