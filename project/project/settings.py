ce28270c3d75c2e571e8e462a94bb6bc2f69d4a7
Sebastian Goldberg <sebas.goldberg@gmail.com>
2017-03-31 17:28:57 -0300

    Fix project language code.

--- project/project/settings.py
+++ project/project/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
